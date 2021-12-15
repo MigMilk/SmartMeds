935d3793f15f109596b5cf661c99eb5b64d34423
AlvaroFrazao <ajgmfrazao@gmail.com>
2021-12-15 21:49:20 +0000

    schema db
    schema

---
+++ db/drawSQL-mysql-export-2021-12-15.sql
@@ -0,0 +1,121 @@
+CREATE TABLE `Login`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `use_class (?)` INT NOT NULL
+);
+ALTER TABLE
+    `Login` ADD PRIMARY KEY `login_id_primary`(`id`);
+CREATE TABLE `Users`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `Nome` INT NOT NULL
+);
+ALTER TABLE
+    `Users` ADD PRIMARY KEY `users_id_primary`(`id`);
+CREATE TABLE `Utente`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `Nome` VARCHAR(255) NOT NULL,
+    `receita_id` INT NOT NULL,
+    `stock_id` INT NOT NULL,
+    `extra_info` VARCHAR(255) NOT NULL,
+    `shared_stock` TINYINT(1) NOT NULL,
+    `stock_id` INT NOT NULL
+);
+ALTER TABLE
+    `Utente` ADD PRIMARY KEY `utente_id_primary`(`id`);
+CREATE TABLE `Receitas`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `nome_med` VARCHAR(255) NOT NULL,
+    `quantity` INT NOT NULL,
+    `intake_period` VARCHAR(255) NOT NULL,
+    `daily_amnt` INT NOT NULL,
+    `intro_date` DATE NOT NULL,
+    `description` VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    `Receitas` ADD PRIMARY KEY `receitas_id_primary`(`id`);
+CREATE TABLE `Stock`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `nome_med` VARCHAR(255) NOT NULL,
+    `per_box_amnt` INT NOT NULL,
+    `total_amnt` INT NOT NULL
+);
+ALTER TABLE
+    `Stock` ADD PRIMARY KEY `stock_id_primary`(`id`);
+CREATE TABLE `User_utente`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `utente_id` INT NOT NULL
+);
+ALTER TABLE
+    `User_utente` ADD PRIMARY KEY `user_utente_id_primary`(`id`);
+CREATE TABLE `utente_receitas`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `utente_id` INT NOT NULL,
+    `receita_id` INT NOT NULL
+);
+ALTER TABLE
+    `utente_receitas` ADD PRIMARY KEY `utente_receitas_id_primary`(`id`);
+CREATE TABLE `Localizaçao`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `piso` INT NOT NULL,
+    `quarto` INT NOT NULL,
+    `corredor` INT NOT NULL
+);
+ALTER TABLE
+    `Localizaçao` ADD PRIMARY KEY `localizaçao_id_primary`(`id`);
+CREATE TABLE `utente_localizaçao`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `utente_id` INT NOT NULL,
+    `locale_id` INT NOT NULL
+);
+ALTER TABLE
+    `utente_localizaçao` ADD PRIMARY KEY `utente_localizaçao_id_primary`(`id`);
+CREATE TABLE `utente_tasks`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `utente_id` INT NOT NULL,
+    `task_id` INT NOT NULL
+);
+ALTER TABLE
+    `utente_tasks` ADD PRIMARY KEY `utente_tasks_id_primary`(`id`);
+CREATE TABLE `tasks`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `task list` VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    `tasks` ADD PRIMARY KEY `tasks_id_primary`(`id`);
+CREATE TABLE `time_table`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `t_start` TIME NOT NULL,
+    `t_end` TIME NOT NULL,
+    `period_name` VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    `time_table` ADD PRIMARY KEY `time_table_id_primary`(`id`);
+CREATE TABLE `utente_stock`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `utente_id` INT NOT NULL,
+    `stock_id` INT NOT NULL
+);
+ALTER TABLE
+    `utente_stock` ADD PRIMARY KEY `utente_stock_id_primary`(`id`);
+ALTER TABLE
+    `User_utente` ADD CONSTRAINT `user_utente_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`);
+ALTER TABLE
+    `utente_receitas` ADD CONSTRAINT `utente_receitas_utente_id_foreign` FOREIGN KEY(`utente_id`) REFERENCES `Utente`(`id`);
+ALTER TABLE
+    `utente_stock` ADD CONSTRAINT `utente_stock_utente_id_foreign` FOREIGN KEY(`utente_id`) REFERENCES `Utente`(`id`);
+ALTER TABLE
+    `User_utente` ADD CONSTRAINT `user_utente_utente_id_foreign` FOREIGN KEY(`utente_id`) REFERENCES `Utente`(`id`);
+ALTER TABLE
+    `utente_stock` ADD CONSTRAINT `utente_stock_stock_id_foreign` FOREIGN KEY(`stock_id`) REFERENCES `Stock`(`id`);
+ALTER TABLE
+    `utente_receitas` ADD CONSTRAINT `utente_receitas_receita_id_foreign` FOREIGN KEY(`receita_id`) REFERENCES `Receitas`(`id`);
+ALTER TABLE
+    `utente_localizaçao` ADD CONSTRAINT `utente_localizaçao_locale_id_foreign` FOREIGN KEY(`locale_id`) REFERENCES `Localizaçao`(`id`);
+ALTER TABLE
+    `utente_localizaçao` ADD CONSTRAINT `utente_localizaçao_utente_id_foreign` FOREIGN KEY(`utente_id`) REFERENCES `Utente`(`id`);
+ALTER TABLE
+    `utente_tasks` ADD CONSTRAINT `utente_tasks_utente_id_foreign` FOREIGN KEY(`utente_id`) REFERENCES `Utente`(`id`);
+ALTER TABLE
+    `utente_tasks` ADD CONSTRAINT `utente_tasks_task_id_foreign` FOREIGN KEY(`task_id`) REFERENCES `tasks`(`id`);
