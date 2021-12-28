2ceb307b81790d292f2af980951bc821c7270aff
Bruno <brunofeitais2011@gmail.com>
2021-12-28 19:39:23 +0000

    Calendário adicionado

--- Application/App/app/src/main/java/com/example/aplicacaopee/calendary.kt
+++ Application/App/app/src/main/java/com/example/aplicacaopee/calendary.kt
@@ -2,10 +2,26 @@ package com.example.aplicacaopee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.EditText
+import android.widget.Toast
 
 class calendary : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendary)
+
+        val calendar = findViewById<CalendarView>(R.id.calendarView)
+        var dia = 0;
+        var mes = 0;
+        var ano = 0;
+
+        calendar.setOnDateChangeListener { calendar, i1, i2, i3 ->
+            dia = i3;
+            mes = i2+1;
+            ano = i1;
+
+            Toast.makeText(this@calendary, "Selected Date: $dia/$mes/$ano", Toast.LENGTH_LONG).show()
+        }
     }
 }
--- Application/App/app/src/main/java/com/example/aplicacaopee/newmenu.kt
+++ Application/App/app/src/main/java/com/example/aplicacaopee/newmenu.kt
@@ -1,12 +1,20 @@
 package com.example.aplicacaopee
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 
 class newmenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        val bsearch = findViewById<Button>(R.id.searchutente)         //search button
+
+        bsearch.setOnClickListener {                                   //handle button click
+            startActivity(Intent(this@newmenu, calendary::class.java))
+        }
     }
 }
