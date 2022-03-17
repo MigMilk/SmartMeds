9b3921eb224f096dca01a3cebfd29fb375d1a025
Bruno <brunofeitais2011@gmail.com>
2022-03-17 23:13:06 +0000

    Comunicacao quase concluida

--- Application/AppFinal/aplicacaoFinal/app/src/main/java/com/example/aplicacaofinal/Login.java
+++ Application/AppFinal/aplicacaoFinal/app/src/main/java/com/example/aplicacaofinal/Login.java
@@ -2,19 +2,22 @@ package com.example.aplicacaofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class Login extends AppCompatActivity {
 
     EditText textInputEditTextUsername, textInputEditTextPassword;
     Button buttonlogin;
-    EditText editTextusername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +31,41 @@ public class Login extends AppCompatActivity {
         buttonlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String username, password;
-                username = String.valueOf(textInputEditTextUsername.getText());
-                password = String.valueOf(textInputEditTextPassword)
+                final String usernamestr, passwordstr;
+                usernamestr = String.valueOf(textInputEditTextUsername.getText());
+                passwordstr = String.valueOf(textInputEditTextPassword.getText());
+
+                if(!usernamestr.equals("") && !passwordstr.equals("")){
+                    Handler handler = new Handler();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String[] field = new String[4];
+                            field[0] = "usernamestr";
+                            field[1] = "passwordstr";
+                            String[] data = new String[2];
+                            data[1] = usernamestr;
+                            data[2] = passwordstr;
+                            PutData putData = new PutData("", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    String result = putData.getResult();
+                                    if (result.equals("Login Success")) {
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "All fields required", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
